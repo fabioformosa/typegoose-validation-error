e24f03ac46fc617b5159a39dad77269234eda591
riccardo.vagelli <riccardo.vagelli@beltos.com>
2019-08-08 14:45:58 +0200

    fix

--- src/model/two/bar.ts
+++ src/model/two/bar.ts
@@ -4,13 +4,13 @@ import * as mongoose from 'mongoose';
 export default class Bar extends Typegoose {
 
   @prop({required : true})
-  fieldThree: string;
-
-  @prop({required : true})
   fieldOne: string;
   
-  @prop({required : true})
+  @prop()
   fieldTwo : string;
+
+  @prop({required : true})
+  fieldThree: string;
 
 }
 
